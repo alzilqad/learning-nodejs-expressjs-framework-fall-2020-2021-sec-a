984d4c3d4836e3ce5bee44c08edf3ae2594e2838
Al Zilqad <alzilqad@gmail.com>
2020-11-17 09:05:32 +0600

    added crud functionality on destination

--- assignment_tour_guide/controllers/place.js
+++ assignment_tour_guide/controllers/place.js
@@ -1,9 +1,93 @@
 const express = require("express");
-const userModel = require.main.require("./models/userModel");
+const placeModel = require.main.require("./models/placeModel");
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-	res.render('place/index', {name: req.cookies['uname'], type: req.cookies['type']});
+router.get("/", (req, res) => {
+  placeModel.getAll(function (results) {
+    res.render("place/index", {
+      place: results,
+      name: req.cookies["uname"],
+      type: req.cookies["type"],
+    });
+  });
+});
+
+router.post("/", (req, res) => {
+  placeModel.getByName(req.body.searchInput, function (results) {
+    console.log(req.body.searchInput);
+    res.render("place/index", {
+      place: results,
+      name: req.cookies["uname"],
+      type: req.cookies["type"],
+    });
+  });
+});
+
+router.get("/create", (req, res) => {
+  res.render("place/create", {
+    name: req.cookies["uname"],
+    type: req.cookies["type"],
+  });
+});
+
+router.post("/create", (req, res) => {
+  var place = [
+    req.body.title,
+    req.body.description,
+    req.body.country,
+    req.body.history,
+    req.cookies["uname"],
+  ];
+  placeModel.insert(place, function (result) {
+    if (result) {
+      res.redirect("/place");
+    } else {
+      res.send("Place has not been Created");
+    }
+  });
+});
+
+router.get("/edit/:id", (req, res) => {
+  placeModel.getById(req.params.id, function (results) {
+    res.render("place/edit", {
+      place: results[0],
+      name: req.cookies["uname"],
+      type: req.cookies["type"],
+    });
+  });
+});
+
+router.post("/edit/:id", (req, res) => {
+  var place = [
+    req.body.title,
+    req.body.description,
+    req.body.country,
+    req.body.history,
+    req.cookies["uname"],
+  ];
+  placeModel.update(req.params.id, place, function (result) {
+    if (result) {
+      res.redirect("/place");
+    } else {
+      res.send("Place has not been Updated");
+    }
+  });
+
+  router.get("/delete/:id", (req, res) => {
+    placeModel.getById(req.params.id, function (results) {
+      res.render("place/delete", {
+        place: results[0],
+        name: req.cookies["uname"],
+        type: req.cookies["type"],
+      });
+    });
+  });
+
+  router.post("/delete/:id", (req, res) => {
+    placeModel.delete(req.params.id, function (results) {
+      res.redirect("/place");
+    });
+  });
 });
 
 module.exports = router;
--- assignment_tour_guide/models/placeModel.js
+++ assignment_tour_guide/models/placeModel.js
@@ -1,3 +1,45 @@
 const db = require("./db");
 
-module.exports = {};
+module.exports = {
+  getById: function (id, callback) {
+    var sql = "select * from attraction where attraction_id='" + id + "'";
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+
+  getAll: function (callback) {
+    var sql = "select * from attraction";
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+
+  insert: function (place, callback) {
+    var sql = `insert into attraction (attraction_id, title, description, country_name, history, username, status) values('', "${place[0]}", '${place[1]}', '${place[2]}', '${place[3]}', '${place[4]}', 'Inactive');`;
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+
+  update: function (id, place, callback) {
+    var sql = `update attraction set title='${place[0]}', description='${place[1]}', country_name='${place[2]}', history='${place[3]}', username='${place[4]}' where attraction.attraction_id='${id}'`;
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+
+  delete: function (id, callback) {
+    var sql = "DELETE from attraction where attraction_id='" + id + "'";
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+
+  getByName: function (name, callback) {
+    var sql = "select * from attraction where country_name like '%" + name + "%'";
+    db.getResults(sql, function (results) {
+      callback(results);
+    });
+  },
+};
